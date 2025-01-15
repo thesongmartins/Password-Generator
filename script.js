d73c912bcbff08ab622b14c8c53c064b75576678
thesongmartins <songmartins01@gmail.com>
2025-01-15 23:16:13 +0100

    Implemeting Js

---
+++ script.js
@@ -0,0 +1,15 @@
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+[]{}|;:',.<>?/";
+
+// Generating random password
+const generatePassword = (
+  length,
+  includeUpper,
+  includeLower,
+  includeNumbers,
+  includeSymbols
+) => {
+  let characters = "";
+};
